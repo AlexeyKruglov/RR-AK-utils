054fb89253defb6d558250fd7290fce2bddd7317
Alexey Kruglov <Alexey.Kruglov@gmail.com>
2013-11-06 11:18:04 +0400

    Seems it would work if collections.deque could block. Now need to implement blocking emulation.

--- RR-proxy-sock.py
+++ RR-proxy-sock.py
@@ -2,14 +2,18 @@
 # Serial port proxy server
 
 import socket,os,io,serial,time
-import threading,collections,Queue
+import threading,collections
 
 socket_name="/tmp/arduinoproxy"
 port_name="/dev/ttyACM0"
 
 
-#sq=collections.deque()  # buffer Arduino output since there's no permanent connection to the socket
-sq=Queue.Queue()  # buffer Arduino output since there's no permanent connection to the socket
+sq=collections.deque()  # buffer Arduino output since there's no permanent connection to the socket
+#sq=Queue.Queue()  # buffer Arduino output since there's no permanent connection to the socket
+
+#    /---->--sock_qu-<----->-\
+#  port                     conn---<<---sock
+#    \---<--------------<----/
 
 def port_reader(port,sock_qu):
   try:
@@ -18,42 +22,58 @@ def port_reader(port,sock_qu):
       time.sleep(0.250)
       line="test. time=%f\n" % time.time()
       print "> %s" % line
-      if conn:
-        try:
-          conn.send(line)
-        except socket.error:
-          print "error!!!"
-          exit()
-      else: sock_qu.put(line)
+      sock_qu.append(line)
   finally:
     #port.close()
     pass
 
 conn=None
-def socket_listener(sock,qu,port):
-  global conn
+conn_ok=threading.Event()  # set when the connection is supposeedly ok
+conn_bad=threading.Event() # not conn_ok -- to be able to wait for bad connention
+conn_ok.clear()
+conn_bad.set()
+
+def socket_connector(sock):
+  global conn, conn_ok, conn_bad
   try:
     while True:
       conn, addr = sock.accept()
       print "[connected]"
-      while True:
-        try:
-          conn.sendall(qu.get_nowait())
-        except Queue.Empty:
-          break
-      while True:
-        data = conn.recv(4096)
-        if not data: break
-        print "> %s" % data
-        if data=="disconnect\n":
-          exit()
-        port.write(data)
+      conn_bad.clear()
+      conn_ok.set()
+      conn_bad.wait()
       print "[disconnected]"
-      conn.close()
-      conn=None
   finally:
     sock.close()
 
+def socket_reader(port):
+  global conn, conn_ok, conn_bad
+  while True:
+    conn_ok.wait()
+    while True:
+      data = conn.recv(4096)
+      if not data: 
+        conn_ok.clear()
+        conn_bad.set()
+        break
+      print "> %s" % data
+      if data=="disconnect\n":
+        exit()
+      port.write(data)
+    conn.close()
+    conn=None
+
+def socket_writer(sock_qu):
+  global conn, conn_ok, conn_bad
+  while True:
+    conn_ok.wait()
+    data=sock_qu.popleft()
+    try:
+      conn.sendall(data)
+    except socket.error:
+      conn_ok.clear()
+      conn_bad.set()
+      sock_qu.appendleft(data)
 
 
 # Open port
@@ -71,7 +91,9 @@ except OSError:
 s.bind(socket_name)
 s.listen(0)
 
-# Start worker processed
-# threading.Thread(target=port_reader,args=(p,sq)).start()
-threading.Thread(target=port_reader,args=(None,sq)).start()
-threading.Thread(target=socket_listener,args=(s,sq,p)).start()
+# Start worker processes
+# threading.Thread(target=port_reader,args=(p,sq),name="port_reader").start()
+threading.Thread(target=port_reader,args=(None,sq),name="port_reader").start()
+threading.Thread(target=socket_writer,args=(sq,),name="socket_writer").start()
+threading.Thread(target=socket_reader,args=(p,),name="socket_reader").start()
+threading.Thread(target=socket_connector,args=(s,),name="socket_connector").start()
