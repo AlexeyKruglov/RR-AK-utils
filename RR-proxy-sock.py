f767483f40ebb8ff7728557c22078bf1e3e2a2e3
Alexey Kruglov <Alexey.Kruglov@gmail.com>
2013-11-07 19:49:53 +0400

    Generally works

--- RR-proxy-sock.py
+++ RR-proxy-sock.py
@@ -1,13 +1,15 @@
 #! /usr/bin/python
 # Serial port proxy server
 
-import socket,os,io,serial,time
+import socket,os,sys,io,serial,time
 import threading,collections
 
 socket_name="/tmp/arduinoproxy"
 port_name="/dev/ttyACM0"
 
+die=threading.Event()  # set to terminate
 
+sqc=threading.Semaphore(0)  # number of elements in sq
 sq=collections.deque()  # buffer Arduino output since there's no permanent connection to the socket
 #sq=Queue.Queue()  # buffer Arduino output since there's no permanent connection to the socket
 
@@ -15,14 +17,15 @@ sq=collections.deque()  # buffer Arduino output since there's no permanent conne
 #  port                     conn---<<---sock
 #    \---<--------------<----/
 
-def port_reader(port,sock_qu):
+def port_reader(port,sock_qu,sock_quc):
   try:
     while True:
       #line=port.readline()
       time.sleep(0.250)
       line="test. time=%f\n" % time.time()
-      print "> %s" % line
+      sys.stdout.write("< " + line)
       sock_qu.append(line)
+      sock_quc.release()
   finally:
     #port.close()
     pass
@@ -48,6 +51,7 @@ def socket_connector(sock):
 
 def socket_reader(port):
   global conn, conn_ok, conn_bad
+  global die
   while True:
     conn_ok.wait()
     while True:
@@ -56,24 +60,30 @@ def socket_reader(port):
         conn_ok.clear()
         conn_bad.set()
         break
-      print "> %s" % data
+      sys.stdout.write("> " + data)
       if data=="disconnect\n":
+        conn.close()
+        port.close()
+        die.set()
         exit()
       port.write(data)
     conn.close()
     conn=None
 
-def socket_writer(sock_qu):
+def socket_writer(sock_qu,sock_quc):
   global conn, conn_ok, conn_bad
   while True:
     conn_ok.wait()
+    sock_quc.acquire()
     data=sock_qu.popleft()
     try:
+      #if conn==None: raise socket.error
       conn.sendall(data)
-    except socket.error:
+    except (socket.error, AttributeError):
       conn_ok.clear()
       conn_bad.set()
       sock_qu.appendleft(data)
+      sock_quc.release()
 
 
 # Open port
@@ -91,9 +101,24 @@ except OSError:
 s.bind(socket_name)
 s.listen(0)
 
+def start_thread(target,args,name,daemon=False):
+  global thread_list
+  th=threading.Thread(target=target,args=args,name=name)
+  #if daemon: 
+  #  th.setDaemon()
+  th.daemon=daemon
+  th.start()
+
+
 # Start worker processes
-# threading.Thread(target=port_reader,args=(p,sq),name="port_reader").start()
-threading.Thread(target=port_reader,args=(None,sq),name="port_reader").start()
-threading.Thread(target=socket_writer,args=(sq,),name="socket_writer").start()
-threading.Thread(target=socket_reader,args=(p,),name="socket_reader").start()
-threading.Thread(target=socket_connector,args=(s,),name="socket_connector").start()
+# threading.Thread(target=port_reader,args=(p,sq,sqc),name="port_reader").start()
+start_thread(target=port_reader,args=(None,sq,sqc),name="port_reader",daemon=True)
+start_thread(target=socket_writer,args=(sq,sqc),name="socket_writer",daemon=True)
+start_thread(target=socket_reader,args=(p,),name="socket_reader")
+start_thread(target=socket_connector,args=(s,),name="socket_connector",daemon=True)
+
+die.wait()
+
+print "[stopping threads]"
+#for th in threading.enumerate():
+#  th.join(0)
--- arduino.py
+++ arduino.py
@@ -12,13 +12,15 @@ def recv_data(s):
   while True:
     try: data_in=s.recv(4096)
     except socket.error: break
+    if not data_in: break
     sys.stdout.write(data_in)
 
+time.sleep(0.1)
 recv_data(s)
 while True:
   data_out=sys.stdin.readline()
   if not data_out: break
-  print "> %s" % data_out
+  sys.stderr.write("> " + data_out)
   s.sendall(data_out)
   recv_data(s)
 
