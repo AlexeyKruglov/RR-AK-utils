a9d2d92152e2ac44f496b8b360ff93afdac5d1ef
Alexey Kruglov <Alexey.Kruglov@gmail.com>
2013-11-11 01:24:49 +0400

    Z probe scanner works.

--- test-marlin.py
+++ test-marlin.py
@@ -12,10 +12,12 @@ class MarlinCmd:  # Marlin commander class
   ard = None
   check_ok = None
   timeout = None
+  echo = None
 
-  def __init__(self, check_ok = True, timeout=None):  # check_ok = raise RRError on non-ok (Error) command results
+  def __init__(self, check_ok = True, timeout=None, echo=sys.stderr):  # check_ok = raise RRError on non-ok (Error) command results
     self.check_ok = check_ok
     self.timeout = timeout
+    self.echo = echo
     self.ard = ArduinoSocketIO(blocking=True, echo=False)
     time.sleep(0.1)
     self.eat_input()
@@ -35,9 +37,10 @@ class MarlinCmd:  # Marlin commander class
       pass
     self.ard.setblocking(True)
 
-  def c(self, cmd, timeout=None, check_ok=None):  # cmd = command without LF
+  def c(self, cmd, timeout=None, check_ok=None, echo=None):  # cmd = command without LF
     if timeout == None: timeout = self.timeout
     if check_ok == None: check_ok = self.check_ok
+    if echo == None: echo = self.echo
 
     self.eat_input()
     self.ard.write(cmd+"\n")
@@ -57,6 +60,8 @@ class MarlinCmd:  # Marlin commander class
       resp2 = resp + " "
       if resp2[0:3]=="ok ":
         return out
+      if echo != None && resp2[0:len("echo:")]=="echo:":
+        echo.write(resp + "\n")
       if resp2[0:len("Error:")]=="Error:":
         if check_ok:
           raise RRError(cmd, resp)
@@ -66,12 +71,17 @@ class MarlinCmd:  # Marlin commander class
           raise RRError(cmd, "Timeout")
         return None
 
-  def go(self, x=None, y=None, z=None):  # !!! assume absolute positioning
+  def set_feedrate(f):
+    self.c("G1 F%.1f" % f*60, check_ok=True)
+
+  def go(self, x=None, y=None, z=None, wait=False):  # !!! assume absolute positioning
     cmd="G1"
     if x != None: cmd += " X%.2f" % x
     if y != None: cmd += " Y%.2f" % y
     if z != None: cmd += " Z%.2f" % z
     self.c(cmd, check_ok=True)
+    if wait:
+      self.wait()
 
   def get_es(self): # return dict with endstop states
     st=self.c("M119", check_ok=True)  # Get endstop status
@@ -81,23 +91,29 @@ class MarlinCmd:  # Marlin commander class
       res[x]=s.upper()!="OPEN"
     return res
 
+  def wait(self, time=0):  # Wait for completion of buffered commands + time seconds
+    self.c("G4 P%.0f" % (time*1e3), check_ok=True)  # Wait
+
   def home(self):
-    print self.c("M80", check_ok=True)  # ATX power on
-    print self.c("G21", check_ok=True)  # Set units to mm
+    self.c("M80", check_ok=True)  # ATX power on
+    self.c("G21", check_ok=True)  # Set units to mm
     es=self.get_es()
     if es["z_min"]:  # too low, go up
-      print self.c("G92 Z0", check_ok=True)  # set logical position
-      print self.c("G1 Z30", check_ok=True)  # Go 30mm up
+      self.c("G92 Z0", check_ok=True)  # set logical position
+      self.c("G1 Z30", check_ok=True)  # Go 30mm up
+      self.wait()
     if es["y_min"]:  # too close, go deeper
-      print self.c("G92 Y0", check_ok=True)  # set logical position
-      print self.c("G1 Y30", check_ok=True)  # Go 30mm deep
+      self.c("G92 Y0", check_ok=True)  # set logical position
+      self.c("G1 Y30", check_ok=True)  # Go 30mm deep
+      self.wait()
     if es["x_max"]:  # too right, go left
-      print self.c("G92 X30", check_ok=True)  # set logical position
-      print self.c("G1 X0", check_ok=True)  # Go 30mm left
-    print self.c("G28", check_ok=True)  # Go home
+      self.c("G92 X30", check_ok=True)  # set logical position
+      self.c("G1 X0", check_ok=True)  # Go 30mm left
+      self.wait()
+    self.c("G28", check_ok=True)  # Go home
 
   def home_z(self):
-    self.c("G1 Z2", check_ok=True)
+    self.go(z=2, wait=True)
     self.c("G28 Z0", check_ok=True)
 
   def probe_z(self, es="z_max", z0=0, maxz=0, minz=-5.5):  # probe vertically with z probe at z_max endstop
@@ -105,28 +121,29 @@ class MarlinCmd:  # Marlin commander class
     self.c("M201 Z200", check_ok=True)  # Maximum z acceleration, mm/s^2
     self.home_z()
     step=0.5
-    delay=0.25*4
+    delay=0.2
     cz=maxz
     # self.c("G1 Z%.2f" % cz, check_ok=True)
     self.go(z=cz)
     time.sleep(delay)
     while not self.get_es()[es] and cz-step>=minz:
       cz -= step
-      print cz
+      #print cz
       # self.c("G1 Z%.2f" % cz, check_ok=True)
       self.go(z=cz)
       time.sleep(delay)
 
     if self.get_es()[es]:
       cz += step
-      #self.home_z()
-      step = 0.1
+      self.home_z()
+      step = 2/46.3
+      cz += step*2
       # self.c("G1 Z%.2f" % cz, check_ok=True)
       self.go(z=cz)
       time.sleep(delay)
       while not self.get_es()[es] and cz-step>=minz:
         cz -= step
-        print cz
+        #print cz
         # self.c("G1 Z%.2f" % cz, check_ok=True)
         self.go(z=cz)
         time.sleep(delay)
@@ -136,7 +153,7 @@ class MarlinCmd:  # Marlin commander class
     return cz+step/2
 
 rr = MarlinCmd()
-
+rr.set_feedrate(100)
 
 
 #ard.write("M114\n")
@@ -153,17 +170,44 @@ rr = MarlinCmd()
 
 rr.home()
 
-time.sleep(1)
-rr.home_z()
+#time.sleep(1)
+#rr.home_z()
 
-exit()
+def probe1(x,y):
+  rr.go(x=x,y=y)
+  z0=rr.probe_z()
+  sys.stdout.write("%.3f %.3f %.3f\n" % (x,y,z0))
+  sys.stdout.flush()
+  return z0
 
-print rr.probe_z()
+def probe_series_r(r):
+  #rr.go(x=r)
+  step_y=45
+  y=20
+  while y<=310:
+    probe1(r,y)
+    y+=step_y
+
+def probe_scan():
+  step_x=32
+  x=133
+  while x<=133 and x>=-30:
+    probe_series_r(x)
+    x-=step_x
+
+try:
+  probe_scan()
+except KeyboardInterrupt:
+  pass
+
+#print "%.2f" % rr.probe_z()
 #rr.go(y=30)
-#print rr.probe_z()
+#print "%.2f" % rr.probe_z()
 #rr.go(y=60)
-#print rr.probe_z()
-##rr.go(123.3,0,0)
+#print "%.2f" % rr.probe_z()
+###rr.go(123.3,0,0)
 #rr.go(123.3,0,0)
 
+rr.go(z=20)
+rr.go(x=123, y=20)
 rr.close()
