4bbd2fd7766832a6d8efa48b23948cfa5560d87b
Alexey Kruglov <Alexey.Kruglov@gmail.com>
2013-11-22 03:14:20 +0400

    1. Improve line drawing; 2. Peano curve drawing code (buggy)
    Line drawing: 1. first position pen, then move it down; 2. Absolute positioning using o constant = origin coordinates.

--- test-geom.py
+++ test-geom.py
@@ -21,15 +21,36 @@ rr.home_z()
 p = rr.get_pos()
 print p
 print p[2]-4
-z0=1.2
+z0=3.3
 z1=z0+5
 
-p=(p[0]-0,p[1]+0,p[2])
-rr.go(x=p[0],y=p[1],z=z0)
-#rr.go(x=p[0],y=p[1]+80,z=z0)
-#rr.go(x=p[0]-80,y=p[1],z=z0)
-rr.go(x=p[0]+80,y=p[1]+80,z=z0)
-#p = rr.get_pos()
+def peano(n,o0,o1, x0,x1,y0,y1):
+  if(n<=0):
+    rr.go(x=o0+x0/2+y0/2,y=o1+x1/2+y1/2,z=z0)
+    return
+  peano(n-1, o0,o1, y0/2,y1/2, x0/2,x1/2)
+  o0+=y0/2
+  o1+=y1/2
+  peano(n-1, o0,o1, x0/2,x1/2, y0/2,y1/2)
+  o0+=x0/2
+  o1+=x1/2
+  peano(n-1, o0,o1, x0/2,x1/2, y0/2,y1/2)
+  o0+=x0/2
+  o1+=x1/2
+  peano(n-1, o0,o1, -y0/2,-y1/2, -x0/2,-x1/2)
+
+o=(182.023,-73.438,6.217)
+
+p=(o[0]-20,o[1]+80,p[2])
+#p=(p[0]+20,p[1]+0,p[2])
+rr.go(x=p[0],y=p[1],z=z1)
+peano(4,p[0],p[1], -20,0, 0,20)
+#exit()
+#rr.go(x=p[0],y=p[1],z=z0)
+#rr.go(x=p[0],y=p[1]+20,z=z0)
+#rr.go(x=p[0]-100,y=p[1],z=z0)
+#rr.go(x=p[0]+10,y=p[1]+80,z=z0)
+p = rr.get_pos()
 rr.go(x=p[0],y=p[1],z=z1)
 
 rr.close()
