5add354844cd063dc21329e34531c61e07fd82b3
Alexey Kruglov <Alexey.Kruglov@gmail.com>
2014-07-17 11:29:23 +0400

    robot_geom.py: update robot geometry
    1) Update geometry parameters:
    1.1) now nozzle position, was pen/probe position;
    1.2) now x axis parallel to table edge, was arbitrary;
    1.3) now origin at near (not at) right front table corner, was at table revolution point.
    2) Temporarily remove table wheel excentricity correction, since it needs recalibration.
    3) Add coordinate naming convention comment

--- robot_geom.py
+++ robot_geom.py
@@ -2,27 +2,43 @@
 
 from math import *
 
+# Coordinate naming convention:
+#  x,y,z  are  Cartesian coordinates
+#  X,Y,Z  are  robot coordinates
+
 class RobotGeometryBase:
   def c2r(self, x,y,z):  # Cartesian to motor coordinate converter
     raise ValueError("RobotGeometryBase r2c() method should be inherited")
 
 class RobotGeometryAK(RobotGeometryBase):
-  # Very approximate yet:
-  #R1=160      # +-10, mm
-  #R2=87       # +-10, mm
-  #Xperp=60.0  # +-5, mm
-  Yperp=272.2*pi/2
-  #phi1k=269   # +-5, mm
-  #phi2k=115   # +-5, mm
-  R1=160.8311
-  R2=76.435
-  Xperp=90.8311
-  phi1k=272.2
-  phi2k=115.573
+  ## Very approximate yet:
+  ##R1=160      # +-10, mm
+  ##R2=87       # +-10, mm
+  ##Xperp=60.0  # +-5, mm
+  #Yperp=272.2*pi/2
+  ##phi1k=269   # +-5, mm
+  ##phi2k=115   # +-5, mm
+  #R1=160.8311
+  #R2=76.435
+  #Xperp=90.8311
+  #phi1k=272.2
+  #phi2k=115.573
+
+  R1              = 158.162         # +/- 1.408        (0.8902%)
+  #Xperp_probe           = 86.0537  #        +/- 2.751        (3.196%)
+  #R2_probe              = 72.9029  #        +/- 1.995        (2.737%)
+  R2              = 94.9426         # +/- 3.049        (3.212%)
+  Xperp           = 65.7656         # +/- 1.868        (2.841%)
+  phi1k           = 270.429         # +/- 2.051        (0.7584%)
+  phi2k           = 108.933         # +/- 3.728        (3.422%)
+  Yperp           = -180.322        # +/- 5.797        (3.215%)
+  xx0             = -235.9          # +/- 2.102        (0.8912%)
+  yy0             = 26.8296         # +/- 2.627        (9.79%)
 
   # additive const: z shift at x=y=0
   #z0  = -6.11831  # mm(Z)
-  z0  = -5.44919
+  #z0  = -5.44919
+  z0=0
 
   def __init__(self):
     pass
@@ -52,12 +68,13 @@ class RobotGeometryAK(RobotGeometryBase):
     py              = 0.00109359
     pxx             = -2.71321e-05
     pxy             = 3.09371e-06
-    ws              = 0.000302164
-    wc              = 0.000452948
+    ws              = 0 * 0.000302164
+    wc              = 0 * 0.000452948
     p2a             = 0.875118
     wp              = 86.7701
 
-    phi1=Y/self.phi1k
+    # No xx0, yy0, Yperp shift here:
+    phi1=(Y           )/self.phi1k
     phi2=(X-self.Xperp)/self.phi2k-pi/2
     x=self.R1*cos(phi1)+self.R2*cos(phi1+phi2)
     y=self.R1*sin(phi1)+self.R2*sin(phi1+phi2)
@@ -70,29 +87,32 @@ class RobotGeometryAK(RobotGeometryBase):
     return Z0
 
   def r2c_approx(self, X,Y,Z):
-    phi1=(Y-self.Yperp)/self.phi1k+pi/2
+    phi1=(Y-self.Yperp)/self.phi1k  #+pi/2
     phi2=(X-self.Xperp)/self.phi2k-pi/2
 
-    x=self.R1*cos(phi1)+self.R2*cos(phi1+phi2)
-    y=self.R1*sin(phi1)+self.R2*sin(phi1+phi2)
+    x=self.R1*cos(phi1) + self.R2*cos(phi1+phi2) + self.xx0
+    y=self.R1*sin(phi1) + self.R2*sin(phi1+phi2) + self.yy0
     z=Z
 
     return (x,y,z)
 
   def c2r_approx(self, x,y,z):
     Z=z
+    x -= self.xx0
+    y -= self.yy0
     r2=x**2+y**2
     r=sqrt(r2)
     phi=atan2(y,x)
     psi1= acos( (self.R1**2+r2-self.R2**2)/(2*self.R1*r))
     psi2=-acos(-(self.R1**2+self.R2**2-r2)/(2*self.R2*self.R1))
     X=(psi2+pi/2)*self.phi2k + self.Xperp
-    Y=(phi+psi1-pi/2)*self.phi1k + self.Yperp
+    # Y=(phi+psi1-pi/2)*self.phi1k + self.Yperp
+    Y=(phi+psi1)*self.phi1k + self.Yperp
     return (X,Y,Z)
 
   def c2r(self, x,y,z):
     X,Y,Z1=self.c2r_approx(x,y,z)
-    print "c2r: X=%f Y=%f" % (X,Y)
+    #print "c2r: X=%f Y=%f" % (X,Y)
     return (X, Y, Z1+self.table_Z0(X,Y))
 
   def r2c(self, X,Y,Z):
